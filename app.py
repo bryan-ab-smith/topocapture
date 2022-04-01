af6898de39f23861b904b702e0d2749e8e5f8082
Bryan Smith <bryanabsmith@gmail.com>
2022-04-01 13:04:23 +1000

    Some fixes to simplify picture for easyocr.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+import cv2
 import easyocr
 from flask import Flask, render_template, request
+import imutils
+import numpy as np
 import requests
 from werkzeug.utils import secure_filename
 
@@ -46,8 +49,8 @@ def getData():
             final_odonyms[key] = value
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def uploadPic():
+@app.route('/upload2', methods=['GET', 'POST'])
+def uploadPic2():
     '''
         To-do:
         1. Run EasyOCR on the picture
@@ -64,11 +67,12 @@ def uploadPic():
 
     # Name and location of the file on the server
     full_file_name = file_dir + secure_filename(upload.filename)
-    
+
     # Save the uploaded file so that it can be OCR'd
     upload.save(full_file_name)
 
-    # Thanks to Nicholas Renotte for the EasyOCR lines (https://www.youtube.com/watch?v=ZVKaWPW9oQY)
+    # Thanks to Nicholas Renotte for the EasyOCR lines
+    # (https://www.youtube.com/watch?v=ZVKaWPW9oQY)
     reader = easyocr.Reader(['en'], gpu=False)
     result = reader.readtext(full_file_name)
     full_text = ''
@@ -78,9 +82,7 @@ def uploadPic():
     os.remove(full_file_name)
 
     for odonym in final_odonyms:
-        #if full_text.split(' ')[0].lower().find(full_text) > 0:
         if full_text.lower().find(odonym.split(' ')[0].lower()) > -1:
-        #if full_text.lower().find(odonym.split(' ')[0].lower() + ' ' + odonym.split(' ')[1][:1].lower()) > 0:
             # return f'{odonym} St, named for {fake_names[odonym]}.'
             return f'''<blockquote class="blockquote">
                 <h1 class="display-6" style="color: #0dcaf0;">{odonym}</h1>
@@ -88,13 +90,63 @@ def uploadPic():
                 <p></p>
                 <p><small>{final_odonyms[odonym][1]}</small></p>
             </blockquote>'''
-    
+
     return 'Nothing found. Try again with a different picture.'
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def uploadPic():
+    '''
+    Huge thanks to Nicholas Renotte's tutorial here
+    https://github.com/nicknochnack/ANPRwithPython/blob/main/ANPR%20-%20Tutorial.ipynb
+    '''
+    # Get the file info
+    upload = request.files.getlist('file')[0]
+
+    # Name and location of the file on the server
+    full_file_name = file_dir + secure_filename(upload.filename)
+
+    # Save the uploaded file so that it can be OCR'd
+    upload.save(full_file_name)
+
+    # Read in the image
+    img = cv2.imread(full_file_name)
+
+    # Convert it to greyscale
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Conevrt size
+    # https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
+    # TBD
+
+    # Thanks to Nicholas Renotte for the EasyOCR lines
+    # (https://www.youtube.com/watch?v=ZVKaWPW9oQY)
+    reader = easyocr.Reader(['en'], gpu=False)
+    result = reader.readtext(grey)
+    full_text = ''
+    for x in result:
+        full_text += ' ' + x[1]
+    print(full_text)
+    os.remove(full_file_name)
+
+    for odonym in final_odonyms:
+        if full_text.lower().find(odonym.split(' ')[0].lower()) > -1:
+            # return f'{odonym} St, named for {fake_names[odonym]}.'
+            return f'''<blockquote class="blockquote">
+                <h1 class="display-6" style="color: #0dcaf0;">{odonym}</h1>
+                <p class="mb-0">{final_odonyms[odonym][0]}</p>
+                <p></p>
+                <p><small>{final_odonyms[odonym][1]}</small></p>
+            </blockquote>'''
+
+    return 'Nothing found. Try again with a different picture.'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == "__main__":
     getData()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
