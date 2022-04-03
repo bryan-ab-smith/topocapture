6c1ecced17ed14ab453524a0e7d567eadf79b526
Bryan Smith <bryanabsmith@gmail.com>
2022-04-03 16:24:18 +1000

    Replaced easyocr.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import cv2
-import easyocr
+# import easyocr
 from flask import Flask, render_template, request
-import imutils
-import numpy as np
+from paddleocr import PaddleOCR
 import requests
 from werkzeug.utils import secure_filename
 
@@ -16,6 +15,7 @@ file_dir = 'static/uploads/'
 final_odonyms = {}
 
 
+@app.before_first_request
 def getData():
     root_url = 'https://bryanabsmith.com/topomapper/datafiles/'
     data_files = [
@@ -40,58 +40,16 @@ def getData():
         to_parse = contents.json()
         for odonym in to_parse['features']:
             # print(to_parse['features']['properties']['name'])
-            odonyms[odonym['properties']['name']] = odonym['properties']['description'], odonym['properties']['refs']
+            odonyms[
+                odonym['properties']['name']
+            ] = odonym['properties']['description'], \
+                odonym['properties']['refs']
 
     print('Removing duplicates...')
 
     for key, value in odonyms.items():
         if value not in final_odonyms:
             final_odonyms[key] = value
-
-
-@app.route('/upload2', methods=['GET', 'POST'])
-def uploadPic2():
-    '''
-        To-do:
-        1. Run EasyOCR on the picture
-            reader = easyocr.Reader(['en'], gpu=False)
-            result = reader.readtext(img)
-            for x in result:
-                print(x[1])
-        2. Look up info in the database/json file
-        3. Render template with info
-    '''
-
-    # Get the file info
-    upload = request.files.getlist('file')[0]
-
-    # Name and location of the file on the server
-    full_file_name = file_dir + secure_filename(upload.filename)
-
-    # Save the uploaded file so that it can be OCR'd
-    upload.save(full_file_name)
-
-    # Thanks to Nicholas Renotte for the EasyOCR lines
-    # (https://www.youtube.com/watch?v=ZVKaWPW9oQY)
-    reader = easyocr.Reader(['en'], gpu=False)
-    result = reader.readtext(full_file_name)
-    full_text = ''
-    for x in result:
-        full_text += ' ' + x[1]
-    print(full_text)
-    os.remove(full_file_name)
-
-    for odonym in final_odonyms:
-        if full_text.lower().find(odonym.split(' ')[0].lower()) > -1:
-            # return f'{odonym} St, named for {fake_names[odonym]}.'
-            return f'''<blockquote class="blockquote">
-                <h1 class="display-6" style="color: #0dcaf0;">{odonym}</h1>
-                <p class="mb-0">{final_odonyms[odonym][0]}</p>
-                <p></p>
-                <p><small>{final_odonyms[odonym][1]}</small></p>
-            </blockquote>'''
-
-    return 'Nothing found. Try again with a different picture.'
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -115,18 +73,15 @@ def uploadPic():
     # Convert it to greyscale
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Conevrt size
-    # https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
-    # TBD
+    # https://github.com/nicknochnack/DrugLabelExtraction-/blob/main/Paddle%20OCR%20Tutorial.ipynb
+    ocr_model = PaddleOCR(lang='en')
+    result = ocr_model.ocr(grey)
+    print(result)
 
-    # Thanks to Nicholas Renotte for the EasyOCR lines
-    # (https://www.youtube.com/watch?v=ZVKaWPW9oQY)
-    reader = easyocr.Reader(['en'], gpu=False)
-    result = reader.readtext(grey)
     full_text = ''
-    for x in result:
-        full_text += ' ' + x[1]
-    print(full_text)
+    for res in result:
+        full_text += ' ' + res[1][0]
+
     os.remove(full_file_name)
 
     for odonym in final_odonyms:
@@ -148,5 +103,4 @@ def index():
 
 
 if __name__ == "__main__":
-    getData()
     app.run(host='0.0.0.0', port=8000, debug=True)
